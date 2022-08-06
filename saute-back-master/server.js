d9bc7131519c2073d0f6400d9697ca985bb804ca
Thomas <thomasdebray83@gmail.com>
2022-08-06 11:28:46 +0200

    Adding helmet

--- saute-back-master/server.js
+++ saute-back-master/server.js
@@ -2,9 +2,11 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const helmet = require("helmet")
 
 //middleware
 app.use(cors())
 app.use(express.json())
+app.use(helmet())
 
 module.exports = { app, express }
